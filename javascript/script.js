b775a490adee8e3ea6943ec9c08b2516a5ded03b
Antônio Marcos Goulart <147720066+Antonio-Marcos-Goulart@users.noreply.github.com>
2025-10-19 19:34:51 -0300

    Ajustes na navegação e estilo: remoção da classe 'active' do item "Início" em várias páginas, adição de onclick para o botão "Faça um Orçamento" e melhorias no estilo do menu.

--- javascript/script.js
+++ javascript/script.js
@@ -6,4 +6,22 @@ $(document).ready(function() {
     $('#mobile_menu_hamburguer .close-btn').click(function() {
         $('#mobile_menu_hamburguer').removeClass('active');
     });
+
+    const sections = $('section');
+    const navItens = $('.nav-item'); 
+});
+
+$(document).ready(function() {
+    // pega o caminho da página atual, ex: "index.html"
+    const path = window.location.pathname.split("/").pop();
+
+    // percorre todos os links do menu principal
+    $('#nav_list .nav-item a, #mobile_nav_list .nav-item a').each(function() {
+        const href = $(this).attr('href');
+
+        // se o href for igual ao path da página atual, adiciona a classe 'active'
+        if (href === path) {
+            $(this).parent().addClass('active');
+        }
+    });
 });
